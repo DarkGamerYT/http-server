45c7410008a7641015d5b446547c1a81f79e5717
King <47607185+DarkGamerYT@users.noreply.github.com>
2025-07-29 19:45:54 +0100

    fix: writing WebSocket payload size

--- src/WebSocket.cpp
+++ src/WebSocket.cpp
@@ -5,7 +5,22 @@
 void WebSocket::send(const std::string& data) const {
     std::string frame;
     frame.push_back(static_cast<char>(0x81)); // FIN + text frame
-    frame.push_back(static_cast<char>(data.size()));
+
+    const size_t payloadSize = data.size();
+    if (payloadSize <= 125) {
+        frame.push_back(static_cast<char>(payloadSize));
+    }
+    else if (payloadSize <= 65535) {
+        frame.push_back(126);
+        frame.push_back(static_cast<char>((payloadSize >> 8) & 0xFF));
+        frame.push_back(static_cast<char>(payloadSize & 0xFF));
+    }
+    else {
+        frame.push_back(127);
+        for (int i = 7; i >= 0; --i)
+            frame.push_back(static_cast<char>((payloadSize >> (8 * i)) & 0xFF));
+    };
+
     frame += data;
 
     HttpServer::sendToSocket(this->mClientSocket, frame);
